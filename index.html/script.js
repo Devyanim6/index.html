123f36c48683f8747a134ee2cacea2e53f485d41
Devyanim6 <130639447+Devyanim6@users.noreply.github.com>
2024-12-27 08:16:07 +0530

    Add files via upload

---
+++ index.html/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Welcome to AutoMarket!');
+    function buyNow() {
+      alert("Thank you for your interest! Our sales team will contact you shortly to complete your purchase.");
+    }
+    
+  });
+  
